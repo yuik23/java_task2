6f53a716992b565ff5d45b572ade187e3daba3b5
yuik23 <yui_karamatsu@yahoo.co.jp>
2023-01-31 00:03:38 +0900

    変数名変更、メソッドで切り出し、替玉数をランダムに変更

--- src/Check.java
+++ src/Check.java
@@ -1,34 +1,45 @@
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Check {
+    private static final Random random = new Random();
+
     public static void main(String[] args) {
-        Ramen ramen1 = new Ramen("豚骨", 800, 0);
-        Ramen ramen2 = new Ramen("醤油", 700, 0);
-        Ramen ramen3 = new Ramen("醤油", 700, 0);
+        List<Ramen> ramenList = createRamenList();
+        List<Customer> customerList = createCustomerList();
 
-        Customer customer1 = new Customer("ぺこぺこ");
-        Customer customer2 = new Customer("ぺこぺこ");
-        Customer customer3 = new Customer("いっぱい");
-
-        ArrayList<Ramen> ramenList = new ArrayList<>();
-        ramenList.add(ramen1);
-        ramenList.add(ramen2);
-        ramenList.add(ramen3);
-
-        ArrayList<Customer> customerList = new ArrayList<>();
-        customerList.add(customer1);
-        customerList.add(customer2);
-        customerList.add(customer3);
-
-//        おなかぺこぺこなら替え玉+1してお会計、いっぱいなら替え玉せずお会計
         for (int i = 0; i < ramenList.size(); i++) {
+
             System.out.println(ramenList.get(i).getTaste() + "らーめん１丁！");
-            if (customerList.get(i).getStomachCondition().equals("ぺこぺこ")) {
-                ramenList.get(i).setKaedama(ramenList.get(i).getKaedama() + 1);
-                System.out.println(i + 1 + "人目のお会計は" + (ramenList.get(i).getPrice() + ramenList.get(i).getKaedama() * 100) + "円です");
+            String customerStomach = customerList.get(i).getStomachCondition();
+
+            if ("ぺこぺこ".equals(customerStomach)) {
+                orderKaedama(ramenList.get(i), random.nextInt(3));
             } else {
                 System.out.println(i + 1 + "人目のお会計は" + ramenList.get(i).getPrice() + "円です");
             }
         }
+    }
+
+    private static List<Ramen> createRamenList() {
+        List<Ramen> ramenList = new ArrayList<>();
+        ramenList.add(new Ramen("豚骨", 800, 0));
+        ramenList.add(new Ramen("醤油", 700, 0));
+        ramenList.add(new Ramen("醤油", 700, 0));
+        return ramenList;
+    }
+
+    private static List<Customer> createCustomerList() {
+        List<Customer> customerList = new ArrayList<>();
+        customerList.add(new Customer("ぺこぺこ"));
+        customerList.add(new Customer("ぺこぺこ"));
+        customerList.add(new Customer("いっぱい"));
+        return customerList;
+    }
+
+    private static void orderKaedama(Ramen ramen, int quantity) {
+        int totalPrice = ramen.getPrice() + (quantity * 100);
+        System.out.println("お会計は" + totalPrice + "円です");
     }
 }
